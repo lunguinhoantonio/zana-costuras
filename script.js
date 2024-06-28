2b0aa965bbc04d90ef514e22042730582a305527
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-06-27 22:15:05 -0300

    ainda em testes

---
+++ script.js
@@ -0,0 +1,12 @@
+// function enviarDados() {
+    
+// }
+// const specificForm = document.getElementsByClassName('formulario-infoClientes');
+
+// specificForm.addEventListener('submit', event => {
+//     event.preventDefault();
+
+//     const dadosForm = new DadosForm(specificForm);
+//     const data = DadosForm.FromEntries(dadosForm);
+//     console.log(data);
+// })
